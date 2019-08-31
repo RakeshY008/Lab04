73272d72f11b66f9734daa414f34fc749a23e708
RakeshY008 <0088@cs>
2019-08-31 12:18:09 +0530

    added method which returns double type value but takes void data type : commit

--- GLAU_2019_BCSC0002_Assignment 2/section1/question7/src/Task.java
+++ GLAU_2019_BCSC0002_Assignment 2/section1/question7/src/Task.java
@@ -1,3 +1,7 @@
 public class Task {
-	// TODO write the method here and delete this line afterwards!
+    // TODO write the method here and delete this line afterwards!
+    public double evaluateExpression(){
+        return ( (4∗10)+5∗3∗(4+2)+10 );
+    }
+
 }
