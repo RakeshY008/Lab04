9aa9acfb706b5af855fade15ffe8d0da91845974
RakeshY008 <0088@cs>
2019-09-02 23:56:25 +0530

    Que01 completed : init commit

--- Lab03/src/Lab03/Que01.java
+++ Lab03/src/Lab03/Que01.java
@@ -6,10 +6,14 @@ public class Que01 {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-
+        HarmonicSeries(n);
     }
     public static void HarmonicSeries(int num){
-
+        double sum = 0.0 ;
+        for (int i=1;i<=num;i++) {
+            sum +=1.0 / i;
+        }
+        System.out.println(sum);
     }
 
 }
