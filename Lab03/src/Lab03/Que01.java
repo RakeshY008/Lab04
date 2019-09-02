f22f0523c8012126ddb540e2c1771b2b7853c306
RakeshY008 <0088@cs>
2019-09-02 23:53:59 +0530

    Harmonic Series method() : init commit

---
+++ Lab03/src/Lab03/Que01.java
@@ -0,0 +1,15 @@
+package Lab03;
+
+import java.util.Scanner;
+
+public class Que01 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+    }
+    public static void HarmonicSeries(int num){
+
+    }
+
+}
