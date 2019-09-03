8c120cfed1f7d2026e4591b7ce61ed3deb954af9
RakeshY008 <0088@cs>
2019-09-03 13:50:47 +0530

    Program1 of Debugging Exercise

---
+++ Lab03/src/programDebugging/Program01.java
@@ -0,0 +1,9 @@
+package programDebugging;
+
+public class Program01 {
+    public static void display() {
+        int x = 123456;
+        float f = 100.12;
+        System.out.println("The float value = " + f);
+    }
+}
