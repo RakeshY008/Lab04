44c261f425da24187d77b5ad8537a65d7324b1c9
RakeshY008 <0088@cs>
2019-09-03 23:43:00 +0530

    Error solved : Program02 of DebuggingExercises

--- Lab03/src/DebuggingExercise/Program02.java
+++ Lab03/src/DebuggingExercise/Program02.java
@@ -1,10 +1,16 @@
 package DebuggingExercise;
-
+/*
+ *@ Author : Rakesh Yadav
+ *@ Version : 1.0
+ */
 
 public class Program02 {
+    public static void main(String[] args) {
+        display();
+    }
 
     public static void display() {
-        int y;
+        int y = 0, x = 11;              //
         if (x > 10) {
             y = x;
         }
