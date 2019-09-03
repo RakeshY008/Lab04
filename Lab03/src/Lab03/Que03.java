0682ff97a0f58bfed5f3c97b8e9b26ebf7a6e966
RakeshY008 <0088@cs>
2019-09-03 12:59:42 +0530

    mainMethod of FahrenheitToCelsius()

--- Lab03/src/Lab03/Que03.java
+++ Lab03/src/Lab03/Que03.java
@@ -4,11 +4,17 @@ package Lab03;
  * @ Version : 1.0
  */
 
+import java.util.Scanner;
+
 public class Que03 {
-
-
-    public double FahrenheitToCelsius(float Fahrenheit) {
-        double C = (Fahrenheit - 32) / 1.8;
+    public static float FahrenheitToCelsius(float Fahrenheit) {
+        float C = (float) ((Fahrenheit - 32) / 1.8);
         return C;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        float F = scanner.nextFloat();
+        System.out.println(FahrenheitToCelsius(F));
     }
 }
