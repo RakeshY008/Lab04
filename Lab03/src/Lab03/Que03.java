c6b8c776a147e9efc16dbabe58c579bfb2d2437e
RakeshY008 <0088@cs>
2019-09-03 07:53:25 +0530

    Added a FahrenheitToCelsius() method

---
+++ Lab03/src/Lab03/Que03.java
@@ -0,0 +1,14 @@
+package Lab03;
+/*
+ *@ Author : Rakesh Yadav
+ * @ Version : 1.0
+ */
+
+public class Que03 {
+
+
+    public double FahrenheitToCelsius(float Fahrenheit) {
+        double C = (Fahrenheit - 32) / 1.8;
+        return C;
+    }
+}
