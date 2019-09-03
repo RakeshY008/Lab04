f4c66b090bb3b18016352204f08bc523cec10da5
RakeshY008 <0088@cs>
2019-09-03 23:22:56 +0530

    Program02 of DebuggingExercises

---
+++ Lab03/src/DebuggingExercise/Program02.java
@@ -0,0 +1,13 @@
+package DebuggingExercise;
+
+
+public class Program02 {
+
+    public static void display() {
+        int y;
+        if (x > 10) {
+            y = x;
+        }
+        System.out.println("The value of Y = " + y);
+    }
+}
