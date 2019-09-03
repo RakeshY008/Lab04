676f52577a6ba21e9d30152cb7db5838d53256ce
RakeshY008 <0088@cs>
2019-09-03 13:21:24 +0530

    Logic to print "Half Right-angled Triangle" pattern Output:
               1
               22
               333
               4444
               55555
               666666
               7777777
               88888888
               999999999

--- Lab03/src/Lab03/Que04.java
+++ Lab03/src/Lab03/Que04.java
@@ -6,12 +6,16 @@ package Lab03;
 
 public class Que04 {
     public static void main(String args[]) {
-        PaternPrint();
+        PaternPrint();                              //method call
     }
 
     public static void PaternPrint() {
-        for (int i = 0; i <= 9; i++) {
-
+        // Logic to print "Half Right-angled Triangle"
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + "\t");
+            }
+            System.out.print("\n");
         }
     }
 }
