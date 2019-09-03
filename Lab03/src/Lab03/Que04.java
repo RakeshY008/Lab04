21e9a16b149cb082418e8b3530b2847ff639a92b
RakeshY008 <0088@cs>
2019-09-03 13:10:14 +0530

    Using For Loop to print "half right angled triangle" E.g,            1            22            333            ....            .....

---
+++ Lab03/src/Lab03/Que04.java
@@ -0,0 +1,17 @@
+package Lab03;
+/*
+ *@ Author : Rakesh Yadav
+ * @ Version : 1.0
+ */
+
+public class Que04 {
+    public static void main(String args[]) {
+        PaternPrint();
+    }
+
+    public static void PaternPrint() {
+        for (int i = 0; i <= 9; i++) {
+
+        }
+    }
+}
