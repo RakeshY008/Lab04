76612b1bcb51ec818eb770d51fb9a76fc7cce206
RakeshY008 <0088@cs>
2019-09-03 23:40:59 +0530

    Program03 of DebuggingExercises

---
+++ Lab03/src/DebuggingExercise/Program03.java
@@ -0,0 +1,10 @@
+package DebuggingExercise;
+
+public class Program03 {
+
+    public static void calculate() {
+        float pi = 3.14f;
+        System.out.println("The value of Pi = " + pi);
+    }
+
+}
