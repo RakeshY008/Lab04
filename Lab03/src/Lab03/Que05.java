2daa2d42307258f9a89c276959f81e56732cc6e4
RakeshY008 <0088@cs>
2019-09-03 13:46:10 +0530

    Main method of class Que05

---
+++ Lab03/src/Lab03/Que05.java
@@ -0,0 +1,28 @@
+package Lab03;
+/*
+ *@ Author : Rakesh Yadav
+ * @ Version : 1.0
+ */
+
+import java.util.Scanner;
+
+public class Que05 {
+    // main method of class Que05
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("date: ");
+        int Date = scanner.nextInt();
+        System.out.print("month: ");
+        int M = scanner.nextInt();
+        System.out.print("year : ");
+        int Year = scanner.nextInt();
+        DMY();
+    }
+
+    public static void DMY() {
+
+
+    }
+
+}
+
