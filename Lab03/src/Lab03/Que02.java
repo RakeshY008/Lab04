f6a61b1c761afd8164598898c7d187abec702881
RakeshY008 <0088@cs>
2019-09-03 07:46:30 +0530

    Calculation for converting amount into Rupees and Paise : e.g, 45.95 = 45 Rupees and 95 Paise

--- Lab03/src/Lab03/Que02.java
+++ Lab03/src/Lab03/Que02.java
@@ -4,8 +4,17 @@ package Lab03;
  * @ Version : 1.0
  */
 
+import java.util.Scanner;
+
 public class Que02 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        float amount = scanner.nextFloat();
+        // Calculation
+        int num = (int) (amount * 100);
+        int paisa = num % 100;
+        int rupya = num / 100;
 
+        System.out.println(rupya + " Rupees and " + paisa + " Paise ");
     }
 }
