e780ef379355e5404e38f0a0db83cfcf8f2c997d
RakeshY008 <0088@cs>
2019-09-03 23:17:52 +0530

    correction in Program01 of DebuggingExercises

---
+++ Lab03/src/DebuggingExercise/Program01.java
@@ -0,0 +1,18 @@
+package DebuggingExercise;
+/*
+ *@ Author : Rakesh Yadav
+ * @ Version : 1.0
+ */
+
+
+public class Program01 {
+    public static void display() {
+        int x = 123456;
+        float f = (float) 100.12;
+        System.out.println("The float value = " + f);
+    }
+
+    public static void main(String[] args) {
+        display();
+    }
+}
--- Lab03/src/programDebugging/Program01.java
+++
@@ -1,9 +0,0 @@
-package programDebugging;
-
-public class Program01 {
-    public static void display() {
-        int x = 123456;
-        float f = 100.12;
-        System.out.println("The float value = " + f);
-    }
-}
