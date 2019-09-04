6dde07edef4b4c3308ce85a975fec96cf9fd9d9a
RakeshY008 <0088@cs>
2019-09-04 15:02:14 +0530

    parametrized constructor call

--- Lab04/src/Lab04/Person.java
+++ Lab04/src/Lab04/Person.java
@@ -44,17 +44,14 @@ public class Person {
         this.gender = gender;
     }
 
+    /*method override
+    public String toString(){
 
-}
-/*
-class Main {
-    public static void main(String[] args) {
-        // create an object of the Person class named myself
-        Person myself = new Person("Dean", 40, 'm');
 
     }
 
- */
+     */
 
+}
 
 
--- Lab04/src/Main.java
+++ Lab04/src/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // create an object of the Person class named myself
         Person myself = new Person("Dean", 40, 'm');
+
+        System.out.println("Hello, my name is " + myself.getName() + " . I am a man and I am " + myself.getAge() + " years old.");
     }
 }
