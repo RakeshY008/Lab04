172cb94c174bdc475cf141250977512b6c26e680
RakeshY008 <0088@cs>
2019-09-04 14:32:52 +0530

    getter and setter methods : commit  class Main : commit

--- Lab04/src/Lab04/Person.java
+++ Lab04/src/Lab04/Person.java
@@ -4,10 +4,43 @@ package Lab04;
  * @ Author : Rakesh Yadav
  * @ Version : 1.0
  */
+
 public class Person {
     private String name;
     private int age;
     private char gender;
 
+    // getter methods
+    public String getName() {
+        return name;
+    }
+
+    // setter method
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Person p1 = new Person();
+
+    }
 
 }
