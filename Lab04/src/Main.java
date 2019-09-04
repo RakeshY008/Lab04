a902d67021910d4f804a05505e0e218f9c2ebc9e
RakeshY008 <0088@cs>
2019-09-04 15:04:14 +0530

    Person with the constructor arguments.

--- Lab04/src/Main.java
+++ Lab04/src/Main.java
@@ -2,7 +2,7 @@ import Lab04.Person;
 
 public class Main {
     public static void main(String[] args) {
-        // create an object of the Person class named myself
+        // set the values of the Person with the constructor arguments.
         Person myself = new Person("Dean", 40, 'm');
 
         System.out.println("Hello, my name is " + myself.getName() + " . I am a man and I am " + myself.getAge() + " years old.");
