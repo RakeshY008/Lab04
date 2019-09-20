46fdadd071c99e63ca2d18e23724ad8f5b77b2c2
RakeshY008 <0088@cs>
2019-09-20 16:49:43 +0530

    Execution Class of Book code challenge

--- GoogleClassRoom/src/Google_Class/Hackerrank.java
+++ GoogleClassRoom/src/Google_Class/Hackerrank.java
@@ -2,6 +2,30 @@
 
 package Google_Class;
 
+import java.util.Scanner;
+
+public class Hackerrank {   //  Execution Class
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        int num = myObj.nextInt();
+        if (num == 0) {
+            System.out.println("N/A");
+        } else {
+            Book[] storeArrey = new Book[num];
+            for (int i = 0; i < num; i++) {
+                String bookname = myObj.nextLine().trim();
+                String authorname = myObj.nextLine().trim();
+                String bsno = myObj.nextLine().trim();
+
+                storeArrey[i] = new Book(bookname, authorname, bsno);
+            }
+            for (Book book : storeArrey) {
+                System.out.println(book);
+            }
+        }
+    }
+}
+
 // Defination Class
 class Book {
     private String bookname;
