803207a1dcee650e6c5cc1f23c2c7ed7c6ad8f07
RakeshY008 <0088@cs>
2019-09-21 10:54:16 +0530

    Book coding challenge of GoogleClassroom.....                                          Commit

--- GoogleClassRoom/src/Google_Class/Hackerrank.java
+++ GoogleClassRoom/src/Google_Class/Hackerrank.java
@@ -1,32 +1,35 @@
 // Coding challenge que. by DBC Sir
 
 package Google_Class;
-
 import java.util.Scanner;
 
 public class Hackerrank {   //  Execution Class
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
         int num = myObj.nextInt();
+        myObj.nextLine().trim();
+        Book[] storeArrey = new Book[num];
         if (num == 0) {
             System.out.println("N/A");
         } else {
-            Book[] storeArrey = new Book[num];
             for (int i = 0; i < num; i++) {
-                String bookname = myObj.nextLine().trim();
-                String authorname = myObj.nextLine().trim();
-                String bsno = myObj.nextLine().trim();
+                String bookname = myObj.nextLine();
+                String authorname = myObj.nextLine();
+                String bsno = myObj.next();
+                myObj.nextLine().trim();
 
                 storeArrey[i] = new Book(bookname, authorname, bsno);
             }
+
             for (Book book : storeArrey) {
                 System.out.println(book);
             }
         }
+        myObj.close();
     }
-}
+    }
 
-// Definition Class
+// Defini tion Class
 class Book {
     private String bookname;
     private String authorname;
@@ -43,27 +46,24 @@ class Book {
         return bookname;
     }
 
-    // Setter method
-    public void setBookname(String bookname) {
-        this.bookname = bookname;
-    }
-
     public String getAuthorname() {
         return authorname;
-    }
-
-    public void setAuthorname(String authorname) {
-        this.authorname = authorname;
     }
 
     public String getBsno() {
         return bsno;
     }
 
+    // Setter method
+    public void setBookname(String bookname) {
+        this.bookname = bookname;
+    }
+    public void setAuthorname(String authorname) {
+        this.authorname = authorname;
+    }
     public void setBsno(String bsno) {
         this.bsno = bsno;
     }
-
 
     @Override
     public String toString() {
