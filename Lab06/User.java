74434f8b4739414ba8deb8936f9f188be4145280
RakeshY008 <rakesh.yadav_cs18@gla.ac.in>
2019-09-25 21:54:55 +0530

    defining getter and setter method in fefinition class

--- Lab06/User.java
+++ Lab06/User.java
@@ -7,5 +7,52 @@ public class User {
     private String email;
     private String password;
 
+    // getter methods
+    public String getName() {
+        return name;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public int getYearOfJoining() {
+        return yearOfJoining;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    // setter methods
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public void setYearOfJoining(int age) {
+        this.yearOfJoining = yearOfJoining;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
+
+
+
 
 }
