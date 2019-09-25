f3cc7b27fbb9163caad3ab01b59da94b71f5db14
RakeshY008 <rakesh.yadav_cs18@gla.ac.in>
2019-09-25 21:47:08 +0530

    variable declaration in definition class i.e.,User class

--- Lab06/User.java
+++ Lab06/User.java
@@ -1,6 +1,11 @@
-import Lab06;
+package Lab06;
 
 public class User {
+    private String name;
+    private String companyName;
+    private int yearOfJoining;
+    private String email;
+    private String password;
 
 
 }
