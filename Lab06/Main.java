ea101dcd45ceaff3296026ed72d736732d3e3a24
Rakesh Yadav <rakesh.yadav_cs18@gla.ac.in>
2019-09-25 23:11:55 +0530

    Delete Main.java

--- Lab06/Main.java
+++
@@ -1,9 +0,0 @@
-import User.java;
-
-public class Main{
-    public static void main(String[] args){
-        User myObj = new User("rakesh","gla",2018);
-        System.out.println(myObj.getEmail());
-        System.out.println(myObj.getPassword());
-    }
-}
